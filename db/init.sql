906bc08e7b0397d4c67f96c5678c430c81caeb39
Mads Andre Lundeland <mads.lundeland@tv2.no>
2022-02-02 22:32:47 +0100

    kafka-streams-app (wip)

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,36 @@
-CREATE TABLE IF NOT EXISTS recipes (
-									   id bigint NOT NULL,
-									   name text NOT NULL,
-									   date_created character varying(255) COLLATE pg_catalog."default",
-									   CONSTRAINT recipes_pkey PRIMARY KEY (id)
+CREATE TABLE author
+(
+    id   bigint NOT NULL,
+    name text   NOT NULL,
+    CONSTRAINT author_pkey PRIMARY KEY (id)
 );
 
+CREATE TABLE book
+(
+    isbn        text   NOT NULL,
+    title       text   NOT NULL,
+    author_id   bigint NOT NULL,
+    description text,
+    CONSTRAINT book_pkey PRIMARY KEY (isbn),
+    CONSTRAINT fk_book_author FOREIGN KEY (author_id) REFERENCES author (id)
+);
 
-INSERT INTO recipes values (1,'Spaghetti bolognese','2021-01-21');
-INSERT INTO recipes values (2,'Fårikål','2021-09-22');
-INSERT INTO recipes values (3,'Bakalao','1999-05-05');
-
--- before is an optional field that if present contains the state of the row before the event occurred.
--- Whether or not this field is available is highly dependent on the REPLICA IDENTITY setting for each table.
 -- REPLICA IDENTITY is a PostgreSQL specific table-level setting which determines the amount of information
 -- that is available to logical decoding in case of UPDATE and DELETE events.
 -- To show the previous values of all the table’s columns, please set the REPLICA IDENTITY level to FULL:
-ALTER TABLE recipes REPLICA IDENTITY FULL;
+ALTER TABLE author
+    REPLICA IDENTITY FULL;
+
+ALTER TABLE book
+    REPLICA IDENTITY FULL;
+
+INSERT INTO author
+values (1, 'Bill Bryson');
+
+INSERT INTO book
+values ('0-7679-0817-1', 'A Short History of Nearly Everything', 1,
+        'A popular science book that explains some areas of science, using easily accessible language that appeals more to the general public than many other books dedicated to the subject.');
+
+INSERT INTO book
+values ('0-7679-0251-3', 'A Walk in the Woods: Rediscovering America on the Appalachian Trail', 1,
+        'A 1997 travel book by the writer Bill Bryson, chronicling his attempt to thru-hike the Appalachian Trail during the spring and summer of 1996');
