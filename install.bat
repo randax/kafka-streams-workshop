7da0d494598919696b5753b01bc2cf7febdbdea2
Erlend Mongstad <erlend.mongstad@tv2.no>
2022-03-18 10:12:23 +0100

    add install.bat for windows users

---
+++ install.bat
@@ -0,0 +1,12 @@
+docker pull debezium/postgres:13
+docker pull node:16
+docker pull openjdk:17.0.1-slim-buster
+docker pull confluentinc/cp-kafka:5.5.6
+docker pull confluentinc/cp-zookeeper:5.5.6
+docker pull confluentinc/cp-schema-registry:5.5.6
+docker pull confluentinc/cp-kafka-connect-base:6.2.1
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.14.2
+docker pull docker.elastic.co/kibana/kibana:7.14.2
+docker pull obsidiandynamics/kafdrop:3.27.0
+
+cd kafka-streams-app && mvnw clean package -DskipTests=true
